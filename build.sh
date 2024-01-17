3534b42c4ce957718d04b5a5c02766e16f0aa05a
Jonatan <hvorblevden@gmail.com>
2024-01-17 09:40:00 +0100

    Removed unused files

--- build.sh
+++
@@ -1,9 +0,0 @@
-echo "Compiling image.cc"
-g++ -Wall -O3 -g -std=c++11 -c -o image.o image.cc
-echo "Compiling Eva.cc"
-g++ -Wall -O3 -g -std=c++11 -c -o Eva.o Eva.cc
-echo "Compiling fbplayer.cc"
-g++ -Wall -O3 -g -std=c++11 -c -o fbplayer.o fbplayer.cc 
-echo "Linking"
-g++ -I. -Wall -O3 -g -std=c++11 -o fbplayer fbplayer.o image.o Eva.o -lm -lpthread 
-echo "Done"
